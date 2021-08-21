9a7367d9d6cef50e9b38bbb4f0f3edc9c6c0bbd7
srikanth2001 <kamparajusrikanth@gmail.com>
2021-08-21 20:05:52 +0530

    codes

---
+++ Assignment-02/codes/assignment2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void collinear(int x1, int y1, int x2,
+               int y2, int x3, int y3)
+{
+
+    int a = x1 * (y2 - y3) +
+            x2 * (y3 - y1) +
+            x3 * (y1 - y2);
+ 
+    if (a == 0)
+        printf("Yes");
+    else
+        printf("No");
+}
+ 
+// Driver Code
+int main()
+{
+    int x1 = 3, x2 = -2, x3 = 8,
+        y1 = 0, y2 = -2, y3 = 2;
+    collinear(x1, y1, x2, y2, x3, y3);
+    return 0;
+}
