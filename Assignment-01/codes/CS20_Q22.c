b4b8e3fc2878d006068d5f482b9ebcf734054cc2
srikanth2001 <kamparajusrikanth@gmail.com>
2021-08-23 14:17:54 +0530

    matrix

--- Assignment-01/codes/CS20_Q22.c
+++ Assignment-01/codes/CS20_Q22.c
@@ -4,6 +4,17 @@ int main(){
                     {6,7,8,9,10},
                     {11,12,13,14,15},
                     {16,17,18,19,20}};
-    printf("%d", *(*(a+**a+2) +3));
+    int i, j;
+
+    for(i = 0; i < 4; i++)
+    {
+        printf("Address of %d th array %u \n",i , *(a + i));
+        for(j = 0; j < 5; j++)
+        {
+             printf("a[%d][%d]=%d\n", i, j, *( *(a + i) + j) );
+        }
+        printf("\n\n");
+    }
+    printf("final output is a[%d][%d]=%d\n", **a+2,3,*(*(a+**a+2) +3));
     return(0);
 }
--- Assignment-02/codes/assignment2.c
+++ Assignment-02/codes/assignment2.c
@@ -5,18 +5,15 @@
 void collinear(int x1, int y1, int x2,
                int y2, int x3, int y3)
 {
-
     int a = x1 * (y2 - y3) +
             x2 * (y3 - y1) +
             x3 * (y1 - y2);
  
     if (a == 0)
-        printf("Yes");
+        printf("Yes this points are collinear");
     else
         printf("No");
 }
- 
-// Driver Code
 int main()
 {
     int x1 = 3, x2 = -2, x3 = 8,
