fd32c513a751d5b694d8f6dcd03df50159d38b7c
srikanth2001 <kamparajusrikanth@gmail.com>
2021-09-08 08:53:49 +0530

    matrixForm

--- Assignment-01/codes/CS20_Q22.c
+++ Assignment-01/codes/CS20_Q22.c
@@ -4,15 +4,16 @@ int main(){
                     {6,7,8,9,10},
                     {11,12,13,14,15},
                     {16,17,18,19,20}};
-    int i, j;
-    for(i = 0; i < 4; i++)
-    {
-        for(j = 0; j < 5; j++)
-        {
-             printf("a[%d][%d]=%d\n", i, j, *( *(a + i) + j) );
-        }
-        printf("\n\n");
-    }
-    printf("final output is a[%d][%d]=%d\n", **a+2,3,*(*(a+**a+2) +3));
+    // int i, j;
+    // for(i = 0; i < 4; i++)
+    // {
+    //     for(j = 0; j < 5; j++)
+    //     {
+    //          printf("a[%d][%d]=%d\n", i, j, *( *(a + i) + j) );
+    //     }
+    //     printf("\n\n");
+    // }
+    // printf("final output is a[%d][%d]=%d\n", **a+2,3,*(*(a+**a+2) +3));
+    printf("ELement with in index *(*(a+**a+2)+3) is a[3][3]  : %d ", a[3][3]);
     return(0);
 }
